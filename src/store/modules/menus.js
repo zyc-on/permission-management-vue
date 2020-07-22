e3375492bcd0fafdfafaaa027f69484a0313cfe6
zyc1o1 <249887404@qq.com>
2020-07-22 17:57:17 +0800

    commit unsaved changes and remove some useless comments

--- src/store/modules/menus.js
+++ src/store/modules/menus.js
@@ -1,9 +1,6 @@
-// import { fetchAllPermissions } from '../../api/menu'
 
 import commonMutations from '../utils/commonMutations'
 import commomActions from '../utils/commonActions'
-
-// import { resolveTreeData } from '../utils/resolveTreeData'
 
 export default {
   namespaced: true,
@@ -20,28 +17,11 @@ export default {
     dialogVisible: false,
     tempItem: {},
     prefix: 'menu'
-    // permissions: []
   },
   mutations: {
     ...commonMutations
-    // setPermissions (state, permissions) {
-    //   state.permissions = permissions
-    // }
   },
   actions: {
     ...commomActions
-    // async getAllPermissions ({ commit }) {
-    //   const res = await fetchAllPermissions()
-    //   commit('setPermissions', res.data.data)
-    // }
   }
-  // getters: {
-  //   mapParentName: state => parentId => {
-  //     const item = state.permissions.find(e => e.id === parentId)
-  //     return item && item.name ? item.name : '无'
-  //   },
-  //   treeData: state => {
-  //     return resolveTreeData(state.permissions)
-  //   }
-  // }
 }
